4b9d46a502eb67bb49f7f4f618ec8eaa8062fdb9
Lieuwe Jan Eilander <lieuwejan@gmail.com>
2013-03-11 20:50:02 +0100

    Removed stuff which cannot be used with the new Kostache version
    - Removed _prepare_view method because it makes no sense if you can't
    	set or bind variables to views
    - Changing the Layout using the response format is not going to work
    	without setting or binding values
    
    I will need to find a different way of doing these things.

--- classes/Ljcore/Controller/Default.php
+++ classes/Ljcore/Controller/Default.php
@@ -25,7 +25,7 @@ abstract class Ljcore_Controller_Default extends Controller {
 
   /**
    * Holds the view object for the current request
-   * @var  Kostache_Layout
+   * @var  View
    */
   public $view;
 
@@ -52,24 +52,6 @@ abstract class Ljcore_Controller_Default extends Controller {
    * @var  array
    */
   protected $_allow_initial_request = TRUE;
-   
-  /**
-   * Hold the response format for this request
-   * @var  string  
-   */
-  protected $_response_format;
-
-  /**
-   * Supported output formats for this controller (specify subdir where templates are found + header accept-type)
-   * 
-   *   '.format' => array(layout, subtemplate, type)
-   *
-   * @var  array
-   */
-  protected $_accept_formats = array(
-    '.html' => array('layout' => 'default', 'subtemplate' => NULL, 'type' => 'text/html'),
-    '.json' => array('layout' => 'json', 'subtemplate' => 'json', 'type' => 'application/json'),
-  );
   
   /**
    * Method which is executed before any action takes place   
@@ -105,77 +87,42 @@ abstract class Ljcore_Controller_Default extends Controller {
     
     if ($this->auto_render === TRUE)
     {
-      // Throw exception if none of the accept-types is not supported
-      $format = $this->request->param('format');
-
-      if ( ! isset($this->_accept_formats[$format]))
-        throw new Http_Exception_415('Unsupported accept-type', NULL);
-    
-      // Set response type
-      $this->_response_format = $this->_accept_formats[$format];
-
       // Set path to view class
       $directory = Request::current()->directory() ? Request::current()->directory().'/' : '';
       $view_path = $directory.Request::current()->controller().'/'.Request::current()->action();
       
+      $class = 'View_'.str_replace('/', '_', $view_path);
+      if ( ! class_exists($class))
+        throw new Http_Exception_404('Page not found');
+
       // Set view object
-      $this->view = $this->_prepare_view($view_path);
+      $this->view = $class;
     }
 
-    // Initialize session (default adapter is database)
-    $this->session = Session::instance();
+    // initialize session (default adapter is database)
+    $this->session = session::instance();
   }
   
   /**
-   * Method which is executed after any action
+   * method which is executed after any action
    * 
    * @return  void
-   * @throws  Http_Exception_404  if view not found 
+   * @throws  http_exception_404  if view not found 
    */
   public function after()
   {
-    if ($this->auto_render === TRUE)
+    if ($this->auto_render === true)
     {
-      // Check if view object is not empty
-      if ($this->view === NULL)
-        throw new Http_Exception_404('Page not found');
+      // check if view object is not empty
+      if ($this->view === null)
+        throw new http_exception_404('page not found');
       
-      // Set header content-type to response format
-      $this->response->headers('Content-Type', $this->_response_format['type']);
-      
-      // Set response body
-      $this->response->body($this->view);
+      // set response body
+      $this->response->body(Kostache_Layout::factory()->render(new $this->view));
     }
     
-    // Execute parent method
+    // execute parent method
     parent::after();
-  }
-  
-  /**
-   * Method to ensure Views use the correct response format for the request
-   * 
-   * @param   string   Requested view path
-   * @param   string   Requested response format
-   * @return  mixed
-   */
-  protected function _prepare_view($view_path)
-  { 
-    // Setup view class
-    $class = 'View_'.str_replace('/', '_', $view_path);
-
-    // Setup full path to template for the view class
-    $full_path = trim($view_path.'/'.$this->_response_format['subtemplate'], '/');
-
-    try
-    {
-      $view = Kostache_Layout::factory()->render(new $class);
-    }
-    catch (Kohana_Exception $e)
-    {
-      // Try to send back the bare template (useful for static content)
-      $view = ($file = Kohana::find_file('templates', $full_path, 'mustache')) ? file_get_contents($file) : NULL;
-    }
-    return $view;
   }
 
   /**
